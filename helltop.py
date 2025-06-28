d1155d4f43af27f80ae9f622c9e828171effb0bc
PolyamSpace <117664621+polyamAdmin@users.noreply.github.com>
2025-06-28 20:52:07 +0200

    Fix lint issues in helltop script
    Signed-off-by: PolyamSpace <117664621+polyamAdmin@users.noreply.github.com>

--- helltop.py
+++ helltop.py
@@ -1,12 +1,22 @@
-import json, operator
+"""Parses hellpot log files and prints top trash-eaters."""
+
+import json
+import operator
 import argparse
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--path', type=Path, default='.', help='specify path to hellpot logs (default: CWD)')
-parser.add_argument('-t', '--top', type=int, default=10, help='show top x results (default: %(default)s)')
-parser.add_argument('-e', '--exclude', type=str, default=[], help='list of IPs to exclude', nargs='+')
-# TODO: Add arguments to get top results since a given date (maybe just since x days) and option to deduplicate results by IP
+parser.add_argument(
+    "-p", "--path", type=Path, default=".", help="specify path to hellpot logs (default: CWD)"
+)
+parser.add_argument(
+    "-t", "--top", type=int, default=10, help="show top x results (default: %(default)s)"
+)
+parser.add_argument(
+    "-e", "--exclude", type=str, default=[], help="list of IPs to exclude", nargs="+"
+)
+# TODO: Add arguments to get top results since a given date (maybe just since x days) and
+# option to deduplicate results by IP
 
 args = parser.parse_args()
 path = args.path
@@ -16,20 +26,20 @@ excluded_ips = args.exclude
 file_count = 0
 full_data = []
 
-for child in Path(path).glob('*.log'):
-  if child.is_file():
-    file_count += 1
-    with open(child) as file:
-      lines = file.readlines()
-      for line in lines:
-        data = json.loads(line)
-        if data['message'] == 'FINISH' and data['REMOTE_ADDR'] not in excluded_ips:
-          full_data.append(data)
+for child in Path(path).glob("*.log"):
+    if child.is_file():
+        file_count += 1
+        with open(child) as file:
+            lines = file.readlines()
+            for line in lines:
+                data = json.loads(line)
+                if data["message"] == "FINISH" and data["REMOTE_ADDR"] not in excluded_ips:
+                    full_data.append(data)
 
-full_data.sort(key=operator.itemgetter('BYTES'), reverse=True)
+full_data.sort(key=operator.itemgetter("BYTES"), reverse=True)
 
 for data in full_data[:top]:
-  data_bytes = round(int(data['BYTES']) / (1000 * 1000 * 1000), 2)
-  print(f'{data["REMOTE_ADDR"]} {data["USERAGENT"]} {data_bytes}GB {data["time"]} {data["URL"]}')
+    data_bytes = round(int(data["BYTES"]) / (1000 * 1000 * 1000), 2)
+    print(f"{data['REMOTE_ADDR']} {data['USERAGENT']} {data_bytes}GB {data['time']} {data['URL']}")
 
 # print(f'Found {file_count} files!')
